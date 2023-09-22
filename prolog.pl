76b10d912e099098b4bc9b5162b5caf53da541aa
joegoico <goicoecheajosem@gmail.com>
2023-09-22 20:50:56 -0300

    prolog en proceso

--- prolog.pl
+++ prolog.pl
@@ -1,9 +1,74 @@
-consulta(hospedaje).
-consulta(traslado).
-consulta(precio).
-consulta(fecha).
-consulta(paquete).
-consulta(destino).
-consulta(reserva).
-consulta(sorpresas).
-consulta(compra).
+destino(1, montania,"Tandil", "Tandil es una ciudad en Argentina conocida por su belleza natural y actividades al aire libre. Ofrece vistas impresionantes de las sierras, una amplia variedad de restaurantes y una rica historia cultural.", "Enlace: https://tandil.tur.ar/").
+destino(2, montania,"San Carlos de Bariloche", "Bariloche es una ciudad en la región de los lagos de Argentina, famosa por su belleza escenica, el esquí y las actividades al aire libre. Ofrece una amplia gama de deportes de aventura y una gran variedad de chocolates y dulces artesanales.", "Enlace: https://barilocheturismo.gob.ar/es/home").
+destino(3, montania,"Mendoza Capital", "Mendoza Capital es una ciudad en Argentina ubicada en la región de Cuyo. Es famosa por sus bodegas y viñedos, asi como por su belleza natural con vistas a la Cordillera de los Andes.", "Enlace: https://mendoza.tur.ar/").
+destino(4, montania,"San Martin de los Andes", "San Martin de los Andes es una encantadora ciudad de montaña en la Patagonia argentina. Ofrece actividades al aire libre, lagos cristalinos y una gran belleza escenica.", "Enlace: http://www.sanmartindelosandes.gov.ar/").
+destino(5, montania,"El Bolson", "El Bolson es una pequeña localidad en la provincia de Rio Negro. Es conocida por su ambiente hippie, sus productos organicos y sus hermosos paisajes en los Andes Patagonicos.", "Enlace: https://www.turismoelbolson.gob.ar/").
+destino(6, montania,"Ushuaia", "Ushuaia es la ciudad más austral del mundo y se encuentra en la Tierra del Fuego. Ofrece paisajes unicos, aventuras en la nieve y cruceros a la Antartida.", "Enlace: https://turismoushuaia.com/").
+destino(7, montania,"Salta", "Salta es una provincia en el noroeste de Argentina con hermosas montañas y paisajes deserticos. Es conocida por su cultura folclórica y sus festivales.", "Enlace: http://www.turismosalta.gov.ar/").
+destino(8, montania,"San Salvador de Jujuy", "San Salvador de Jujuy es la capital de la provincia de Jujuy. Ofrece montañas, paisajes coloridos, y una rica tradición cultural y gastronomica.", "Enlace: http://www.turismo.jujuy.gob.ar/").
+
+destino(9, playa,"Mar del Plata", "Mar del Plata es una ciudad costera en la provincia de Buenos Aires. Es famosa por sus playas, vida nocturna y actividades acuaticas.", "Enlace: https://www.turismomardelplata.gob.ar/").
+destino(10, playa,"Puerto Madryn", "Puerto Madryn se encuentra en la Patagonia argentina y es conocida por su fauna marina, especialmente ballenas y pingüinos. También ofrece playas hermosas.", "Enlace: https://www.puertomadrynturismo.com.ar/").
+destino(11, playa,"Pinamar", "Pinamar es una localidad de playa en Buenos Aires con hermosas dunas de arena y una gran vida nocturna. Es un destino de moda para los veraneantes.", "Enlace: https://pinamar.tur.ar/").
+destino(12, playa,"Monte Hermoso", "Monte Hermoso es una tranquila localidad costera en la provincia de Buenos Aires. Ofrece playas extensas y un ambiente relajado para vacacionar.", "Enlace: https://montehermoso.gov.ar/sitio/").
+destino(13, playa,"Las Grutas", "Las Grutas es una localidad en Rio Negro con formaciones de roca unicas y playas de aguas calidas. Es ideal para el buceo y la exploracion submarina.", "Enlace: https://lasgrutasturismo.gob.ar/").
+destino(14,playa, "Carilo", "Cariló es una ciudad de playa en Buenos Aires con un ambiente familiar y bosques de pinos. Ofrece playas tranquilas y un estilo de vida relajado.", "Enlace: https://venialacosta.com/informacion-detalles.php?id=3683").
+destino(15 , playa, "Mar de las Pampas", "Mar de las Pampas es una localidad costera rodeada de bosques y dunas en Buenos Aires. Es un refugio natural para aquellos que buscan paz y tranquilidad.", "Enlace: https://www.mardelaspampas.com.ar/").
+
+destino(16,urbano, "Buenos Aires", "Buenos Aires es la capital de Argentina y una ciudad cosmopolita llena de cultura, arte, arquitectura y vida nocturna. Ofrece una experiencia urbana única.", "Enlace: https://turismo.buenosaires.gob.ar/es").
+destino(17,urbano, "Rosario", "Rosario es una ciudad a orillas del río Paraná conocida por su vibrante vida cultural y arquitectura. Es un importante centro industrial y cultural.", "Enlace: https://rosario.tur.ar/").
+destino(18, urbano, "Cordoba Capital", "Córdoba Capital es la ciudad más grande de la provincia de Córdoba. Ofrece historia, universidades y una gran vida nocturna, así como hermosos paisajes serranos cercanos.", "Enlace: https://turismo.cordoba.gob.ar/").
+
+
+traslado(av01, "Aerolineas Argentinas","Enlace: www.aerolineas.com.ar, Teléfono: 0810-222-86527").
+traslado(av02, "Jet Smart","Enlace: www.jetsmart.com, Teléfono: 11-2206-7799").
+traslado(av04, "Flybondi","Enlace: www.flybondi.com, Teléfono: 351-284-3320").
+traslado(av05, "Lade","Enlace:www.lade.com.ar, Teléfono:0810-810-5233").
+
+hotel(1, 'Hotel Plaza', 4, 2, 'Centro', 150).
+hotel(2, 'Hotel Estrella', 3, 3, 'Playa', 120).
+hotel(3, 'Hotel Montaña', 4, 2, 'Montaña', 160).
+hotel(4, 'Hotel del Mar', 5, 2, 'Playa', 200).
+hotel(5, 'Hotel Ciudad', 3, 2, 'Centro', 100).
+hotel(6, 'Hotel Tranquilo', 2, 2, 'Montaña', 80).
+hotel(7, 'Hotel Playa Blanca', 4, 3, 'Playa', 180).
+hotel(8, 'Hotel Céntrico', 3, 1, 'Centro', 90).
+hotel(9, 'Hotel Sol', 2, 2, 'Playa', 70).
+hotel(10, 'Hotel Montañés', 3, 3, 'Montaña', 120).
+hotel(11, 'Hotel del Bosque', 4, 2, 'Montaña', 150).
+hotel(12, 'Hotel Playa Dorada', 4, 3, 'Playa', 170).
+hotel(13, 'Hotel del Centro', 3, 2, 'Centro', 110).
+hotel(14, 'Hotel Estrellado', 2, 2, 'Playa', 75).
+hotel(15, 'Hotel de la Montaña', 3, 2, 'Montaña', 125).
+hotel(16, 'Hotel Costero', 4, 3, 'Playa', 190).
+hotel(17, 'Hotel del Este', 3, 2, 'Centro', 105).
+hotel(18, 'Hotel de la Playa', 2, 2, 'Playa', 85).
+hotel(19, 'Hotel del Sol', 4, 2, 'Playa', 160).
+hotel(20, 'Hotel del Norte', 3, 3, 'Montaña', 130).
+
+consultar_hoteles(Codigo, Nombre, Estrellas, Camas, Ubicacion, Precio):- 
+    hotel(Codigo, Nombre, Estrellas, Camas, Ubicacion, Precio),
+    write('Codigo hotel: '), write(Codigo),nl,
+    write('Nombre del hotel: '), write(Nombre),nl,
+    write('Estrellas: '), write(Estrellas),nl,
+    write('Cantidad de camas: '), write(Camas),nl,
+    write('Ubicacion: '),write(Ubicacion),nl,
+    write('Precio por noche en dolares: $'),write(Precio), nl,nl,
+    fail.
+
+
+consultar_destinos(Codigo, Tipo, Nombre, Descripcion, Enlace) :-
+     destino(Codigo, Tipo, Nombre, Descripcion, Enlace),
+    write('Nombre: '), write(Nombre), nl,
+    write('Codigo de la ciudad: '), write(Codigo),nl,nl,
+    fail.
+
+consultar_traslados(Codigo, Nombre, Enlace):- 
+    traslado(Codigo, Nombre, Enlace),
+    write('Codigo Aerolinea: '), write(Codigo),nl,
+    write('Nombre de la Aerolinea: '), write(Nombre),nl,
+    write('Enlace a la pagina de la aerolinea: '), write(Enlace),nl,nl,
+    fail.
+
+
+
